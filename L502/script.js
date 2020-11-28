e58b5ce506a1fe6b8d4faf9a8dab768accaf526d
meliodasjr <luka.onfroy@gmai.com>
2020-11-28 16:40:47 +0400

    L502

---
+++ L502/script.js
@@ -0,0 +1,9 @@
+let checkElement = document.getElementById('empty-list');
+
+if(checkElement.childElementCount == 0)
+{
+    console.log('elemment is empty!');
+}
+else{
+    console.log('element is not! empty!');
+}
